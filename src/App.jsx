3cf9fdc6067220ad8e2e4a68f8f08ea5936d0375
Augustine <augustinestack@gmail.com>
2024-10-26 10:51:16 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import deserts from './assets/json/deserts.json';
+import image from './assets/images/image-baklava-desktop.jpg'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main>
+      <div className='container'>
+        <div className='left'>
+          {deserts.map((desert, index) => (
+            <div className='desert-col' key={index}>
+              <div className="desert-img">
+                <img src={image} alt="" />
+                <button>Add to cart</button>
+              </div>
+              <div className="desert-content">
+                <h3>{desert.category}</h3>
+                <h2>{desert.title}</h2>
+                <span>{desert.price}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className='right'></div>
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
