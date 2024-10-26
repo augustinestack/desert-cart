334486c3ebcc027ee35d3f150dca89b214f7e1d2
Augustine <augustinestack@gmail.com>
2024-10-26 11:45:05 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import deserts from './assets/json/deserts.json';
-import image from './assets/images/image-baklava-desktop.jpg'
+import image from './assets/images/image-baklava-desktop.jpg';
 
 const App = () => {
   return (
     <main>
       <div className='container'>
         <div className='left'>
-          {deserts.map((desert, index) => (
-            <div className='desert-col' key={index}>
-              <div className="desert-img">
-                <img src={image} alt="" />
-                <button>Add to cart</button>
+          <h1>Desserts</h1>
+          <div className='left-row'>
+            {deserts.map((desert, index) => (
+              <div className='desert-col' key={index}>
+                <div className='desert-img'>
+                  <img src={image} alt='' />
+                  <button>Add to cart</button>
+                </div>
+                <div className='desert-content'>
+                  <h3>{desert.category}</h3>
+                  <h2>{desert.title}</h2>
+                  <span>{desert.price}</span>
+                </div>
               </div>
-              <div className="desert-content">
-                <h3>{desert.category}</h3>
-                <h2>{desert.title}</h2>
-                <span>{desert.price}</span>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
         <div className='right'></div>
       </div>
